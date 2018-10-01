e079bbf32ef6e82b68efdc6d6ecf9637f0ec8a07
ArtursVed <42648272+ArtursVed@users.noreply.github.com>
2018-10-01 11:31:53 +0000

    Update Person.java

--- src/notepad/Person.java
+++ src/notepad/Person.java
@@ -1,55 +1,105 @@
-package notepad;
+package notepad_vb1;
+
 
 public class Person {
+
     private static int count = 0;
+
     private int id;  // private --prjacet peremennie klassa
     private String name;
+
     private String surname;
+
     private String phone;
+
+    private String email;
+
+    private String Exit;
+
+    private String Create;
+
+    private String Delete;
+
+    int id_num;
+
 
 
     public Person() {
+
         count ++;
+
         id = count;
+
     }
 
+
     public int getId() {
+
         return id;
+
     }
 
 
     public String getName() {
 
+
         return name;
+
     }
 
+
     public void setName(String name) {
+
         this.name = name;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getSurname() {
+
         return surname;
+
     }
+
 
     public void setSurname(String surname) {
+
         this.surname = surname;
+
     }
+
 
     public String getPhone() {
+
         return phone;
+
     }
+
+
 
     public void setPhone(String phone) {
+
         this.phone = phone;
+
     }
 
-    @Override
+    @Override   // konvertacija objecta v stroku
     public String toString() {
         return "Person{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", phone='" + phone + '\'' +
-                '}' + count;
+                ", email='" + email + '\'' +
+                '}';
     }
+
+
 }

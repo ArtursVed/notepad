635a2e2534e31f5585048f87878311d66479be26
ArtursVed <42648272+ArtursVed@users.noreply.github.com>
2018-10-09 11:57:14 +0300

    Update Reminder.java

--- src/notepad/Reminder.java
+++ src/notepad/Reminder.java
@@ -1,1 +1,45 @@
+package notepad;
+
+public class Reminder extends Note {
+
+
+
+    private String date;
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Reminder{" +
+                "id=" + getId() + '\'' +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean hasSubstring(String str) {
+        return date.contains(str)
+        ||  time.contains(str);
+
+    }
+}
 

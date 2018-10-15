5c32d27f3c32c0232b849ddef689e6e6ea9c34c9
ArtursVed <42648272+ArtursVed@users.noreply.github.com>
2018-10-15 15:31:22 +0300

    Update Reminder.java

--- src/notepad/Reminder.java
+++ src/notepad/Reminder.java
@@ -1,23 +1,14 @@
 package notepad;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
-public class Reminder extends Note {
+
+public class Reminder extends Alarm {
 
 
 
     private LocalDate date;
-    private LocalTime time;
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
+  
 
 
 
@@ -33,9 +24,9 @@ public class Reminder extends Note {
     public String toString() {
         return "Reminder{" +
                 "id=" + getId() + '\'' +
-                "note_text='" + getNote_text() + '\'' +
+                "time='" + getTime() + '\'' +
                 "date='" + date.format(Main.DATE_FORMATER) + '\'' +   // mozno otformatirovat po svoim dannim
-                ", time='" + time.format(Main.TIME_FORMATTER) + '\'' +
+              
                 '}';
     }
 
@@ -44,7 +35,7 @@ public class Reminder extends Note {
     public boolean hasSubstring(String str) {
         return super.hasSubstring(str)
         ||  date.format(Main.DATE_FORMATER).contains(str)
-        ||  time.format(Main.TIME_FORMATTER).contains(str);
+    
 
 
     }
@@ -54,9 +45,7 @@ public class Reminder extends Note {
 
         System.out.println("Enter date"); // sprasivaem
         date = Main.askDate();// scitivaem s ekrana
-        System.out.println("Enter time");
-        time =  Main.askTime();
-
+  
 
     }
 
